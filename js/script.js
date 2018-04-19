1e3eaaabaad96325611dc8b82447643ccaf9c026
lulutor <lucas.nandan@gmail.com>
2018-04-19 11:57:16 +0200

    admin progress js

--- js/script.js
+++ js/script.js
@@ -2,16 +2,26 @@
 var users = [
   {userName:"admin", password:"password"}
 ]
-var userNameSignIn = document.getElementById("username").value;
-var passwordSignIn = document.getElementById("password").value;
 var lienSignIn = document.getElementById("lien_validation");
+var validate = document.getElementById("validate")
+var signUp = document.getElementById("sign-up")
 
-var validation = function (utilitaire, motDePasse) {
-  for(var i=0; i<users.length; i++){
-    if(utilitaire === users[i].userName && motDePasse === users[i].userName) {
-      lienSignIn.setAttribute("href", "pages/dashboard.html");
+var validation = function (utilisateur, motDePasse) {
+  for(var i = 0; i < users.length; i++) {
+    if(utilisateur === users[i].userName && motDePasse === users[i].password) {
+      lienSignIn.setAttribute("href", "pages/dashboard.html")
     }
   }
+  return;
 }
+validate.addEventListener('click', function(){
+  var userNameSignIn = document.getElementById("username").value;
+  var passwordSignIn = document.getElementById("password").value;
+  validation(userNameSignIn, passwordSignIn);
+});
 
-validation(userNameSignIn, passwordSignIn);
+signUp.addEventListener('click', function() {
+  var userNameSignIn = document.getElementById("username").value;
+  var passwordSignIn = document.getElementById("password").value;
+  users.push({userName: userNameSignIn, password: passwordSignIn});
+});
