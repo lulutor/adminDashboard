e765d18afdc3e43be7c26482c3ba52a1254ac56d
lulutor <lucas.nandan@gmail.com>
2018-04-20 09:09:17 +0200

    ajout message acceuil admin

--- js/script.js
+++ js/script.js
@@ -4,12 +4,15 @@ var users = [
 ]
 var lienSignIn = document.getElementById("lien_validation");
 var validate = document.getElementById("validate")
-var signUp = document.getElementById("sign-up")
+var signUp = document.getElementById("sign_up")
 
 var validation = function (utilisateur, motDePasse) {
   for(var i = 0; i < users.length; i++) {
     if(utilisateur === users[i].userName && motDePasse === users[i].password) {
       lienSignIn.setAttribute("href", "pages/dashboard.html")
+      document.getElementById("you_can").textContent = "You can sign in"
+    }else{
+      document.getElementById("you_can").textContent = "Wrong password"
     }
   }
   return;
@@ -18,10 +21,11 @@ validate.addEventListener('click', function(){
   var userNameSignIn = document.getElementById("username").value;
   var passwordSignIn = document.getElementById("password").value;
   validation(userNameSignIn, passwordSignIn);
+
 });
 
 signUp.addEventListener('click', function() {
-  var userNameSignIn = document.getElementById("username").value;
-  var passwordSignIn = document.getElementById("password").value;
-  users.push({userName: userNameSignIn, password: passwordSignIn});
-});
+  var userNameSignUp = document.getElementById("username_sign_up").value;
+  var passwordSignUp = document.getElementById("password_sign_up").value;
+  users.push({userName: userNameSignUp, password: passwordSignUp});
+})
